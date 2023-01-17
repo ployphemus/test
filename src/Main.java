c9cfe1dc98f3f3551695f54ba961b3054cecb267
kipVaughan <wovaughan@uncg.edu>
2023-01-17 10:17:25 -0500

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String version = "0.01";
+        char finish = 'n';
+        while(finish != 'y') {
+            System.out.println("Fuel System " + version + "\n");
+            //select user role, will be replaced with login screen
+            System.out.println("Please select role: \n1. Admin\n2. User\n3. Fuel Truck");
+            Scanner input = new Scanner(System.in);
+            input.close();
 
-        System.out.println("Hello world!");
-        int t = 5;
-        t = t + 6;
-
+            finish = 'y';
+        }
     }
 }
